f89e8c78d7e317ff8579d16d8bf789cd28b39e24
fabricia <fabricia.assis@aluno.ifsp.edu.br>
2022-12-14 23:56:54 -0300

    identação

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
-function showModal(idModal){
+function showModal(idModal) {
     const modal = document.querySelector(idModal)
     modal.style.display = 'flex'
 }
 
-function hideModal(idModal, event){
-    if(event.target.className === 'modal'){
-            const modal = document.querySelector(idModal)
-            modal.style.display = 'none'
+function hideModal(idModal, event) {
+    if (event.target.className === 'modal') {
+        const modal = document.querySelector(idModal)
+        modal.style.display = 'none'
 
     }
 }
 
-function closeAllModal(){
+function closeAllModal() {
     const modais = document.querySelectorAll('.modal')
     modais.forEach(modal => {
         modal.style.display = 'none'
@@ -19,17 +19,17 @@ function closeAllModal(){
     })
 }
 
-async function insert(event){
+async function insert(event) {
     event.preventDefault() //para não recarregar a pag
-    const formData = new FormData(event.target) 
+    const formData = new FormData(event.target)
     const response = await fetch('backend/insert.php', { //fetch: oq faz o js desparar para o backend sem recarregar a pag
         method: 'POST', //await: a resposta do fetch não é automatica por isso o await
         body: formData
     })
     const result = await response.json()
-    if(result?.success){
+    if (result?.success) {
         closeAllModal()
-        alert('Sua incrição '+result.data.nome+' foi feita com sucesso!')
+        alert('Sua incrição ' + result.data.nome + ' foi feita com sucesso!')
         loadProductions()
     }
 }
@@ -37,20 +37,19 @@ async function insert(event){
 async function loadProductions() {
     const response = await fetch('backend/list.php')
     const result = await response.json()
-    if(result?.success){
+    if (result?.success) {
         const listProductions = document.querySelector('#productions')
         listProductions.innerHTML = ''
         const insc = result.data
         insc.map((insc) => { //map é um laço de repeição
             listProductions.innerHTML += `
                 <div class="card-insc">
-                    <a href="insc">
+                    
                     <h4>${insc.nome}</h4>
-                    </a>
+                    
                     <div>
-                        <a href="livro">
+                       
                             <h5>${insc.cpf}</h5>
-                        </a>
                         <div>
                             <p>${insc.dataNas}</p>
                             <p>${insc.email}</p>
@@ -61,25 +60,25 @@ async function loadProductions() {
                 </div>
         `
         })
-    }else{
+    } else {
         alert('Erro ao carregar produções')
     }
 }
 
-async function deleteProduction(id){
-    const response = await fetch('backend/delete.php?id='+id)
+async function deleteProduction(id) {
+    const response = await fetch('backend/delete.php?id=' + id)
     const result = await response.json()
-    if(result?.success){
+    if (result?.success) {
         alert('Sua inscrição foi deletada com sucesso!')
         loadProductions()
     }
 
 }
 
-async function loadProductionData(id){
-    const response = await fetch('backend/get-production-by-id.php?id='+id)
+async function loadProductionData(id) {
+    const response = await fetch('backend/get-production-by-id.php?id=' + id)
     const result = await response.json()
-    if(result?.success){
+    if (result?.success) {
         showModal('#modal-editar')
         const nome = document.querySelector('#modal-editar input[name=fullName]')
         nome.value = result.data.nome
@@ -97,17 +96,17 @@ async function loadProductionData(id){
 
 }
 
-async function edit(event){
+async function edit(event) {
     event.preventDefault()
-    const formData = new FormData(event.target) 
-    const response = await fetch('backend/edit.php', { 
+    const formData = new FormData(event.target)
+    const response = await fetch('backend/edit.php', {
         method: 'POST',
         body: formData
     })
     const result = await response.json()
-    if(result?.success){
+    if (result?.success) {
         closeAllModal()
-        alert('Sua inscrição '+result.data.titulo+' foi editada com sucesso!')
+        alert('Sua inscrição ' + result.data.titulo + ' foi editada com sucesso!')
         loadProductions()
     }
 }
